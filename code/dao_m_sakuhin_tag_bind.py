7b0879e00004456828a134e575edd64fbe90daaa
huangsx_glb <313300497@qq.com>
2020-03-08 23:17:57 +0900

    code

---
+++ code/dao_m_sakuhin_tag_bind.py
@@ -0,0 +1,278 @@
+from admin_app.dao.dao_main import DaoMain
+
+class DaoMTagBind(DaoMain):
+
+
+    def selectMangaTagByTitleName(self, param):
+        sql = """
+               select
+                    manga.manga_title_code
+                    , manga.manga_title_name
+                    , 'マンガ' AS category_name
+                    , '01' AS category_code
+                    , manga.update_user
+                    , manga.update_time
+                    , mp.sakuhin_tag_code1
+                    , mp.sakuhin_tag_code2
+                    , mp.sakuhin_tag_code3
+                    , mp.sakuhin_tag_code4
+                    , mp.sakuhin_tag_code5
+                    , mp.sakuhin_tag_code6
+                    , mp.sakuhin_tag_code7
+                    , mp.sakuhin_tag_code8
+                    , mp.sakuhin_tag_code9
+                    , mp.sakuhin_tag_code10
+                    , mp.sakuhin_tag_code11
+                    , mp.sakuhin_tag_code12
+                    , mp.sakuhin_tag_code13
+                    , mp.sakuhin_tag_code14
+                    , mp.sakuhin_tag_code15
+                    , mp.sakuhin_tag_code16
+                    , mp.sakuhin_tag_code17
+                    , mp.sakuhin_tag_code18
+                    , mp.sakuhin_tag_code19
+                    , mp.sakuhin_tag_code20
+                    , manga.tag_map_code
+                from
+                    m_manga_title manga 
+                    left join m_sakuhin_tag_map mp 
+                        on mp.tag_map_code = manga.tag_map_code 
+                        and manga.invalid_flg = false 
+                where
+                    manga_title_name LIKE %(title_name)s
+        """
+        return self.selectWithParam(sql, param)
+
+    def getTagNameByCode(self,param):
+        sql = """
+            select
+                sakuhin_tag_code,
+                sakuhin_tag_name 
+            from
+                m_sakuhin_tag 
+            where
+                sakuhin_tag_code in %(tag_code_list)s
+
+            """
+        
+        return self.selectWithParam(sql, param)
+
+    def selectNovelTagByTitleName(self, param):
+        sql = """
+               select
+                    novel.novel_title_code
+                    , novel.novel_title_name
+                    , '小説' AS category_name
+                    , '02' AS category_code
+                    , novel.update_user
+                    , novel.update_time
+                    , mp.sakuhin_tag_code1
+                    , mp.sakuhin_tag_code2
+                    , mp.sakuhin_tag_code3
+                    , mp.sakuhin_tag_code4
+                    , mp.sakuhin_tag_code5
+                    , mp.sakuhin_tag_code6
+                    , mp.sakuhin_tag_code7
+                    , mp.sakuhin_tag_code8
+                    , mp.sakuhin_tag_code9
+                    , mp.sakuhin_tag_code10
+                    , mp.sakuhin_tag_code11
+                    , mp.sakuhin_tag_code12
+                    , mp.sakuhin_tag_code13
+                    , mp.sakuhin_tag_code14
+                    , mp.sakuhin_tag_code15
+                    , mp.sakuhin_tag_code16
+                    , mp.sakuhin_tag_code17
+                    , mp.sakuhin_tag_code18
+                    , mp.sakuhin_tag_code19
+                    , mp.sakuhin_tag_code20
+                    , novel.tag_map_code 
+                from
+                    m_novel_title novel 
+                    left join m_sakuhin_tag_map mp 
+                        on mp.tag_map_code = novel.tag_map_code 
+                        and novel.invalid_flg = false 
+                where
+                    novel_title_name LIKE %(title_name)s
+        """
+        return self.selectWithParam(sql, param)
+
+    def selectAnimeTagByTitleName(self, param):
+        sql = """
+            select
+                    anime.anime_title_code
+                    , anime.anime_title_name
+                    , 'アニメ' AS category_name
+                    , '03' AS category_code
+                    , anime.update_user
+                    , anime.update_time
+                    , mp.sakuhin_tag_code1
+                    , mp.sakuhin_tag_code2
+                    , mp.sakuhin_tag_code3
+                    , mp.sakuhin_tag_code4
+                    , mp.sakuhin_tag_code5
+                    , mp.sakuhin_tag_code6
+                    , mp.sakuhin_tag_code7
+                    , mp.sakuhin_tag_code8
+                    , mp.sakuhin_tag_code9
+                    , mp.sakuhin_tag_code10
+                    , mp.sakuhin_tag_code11
+                    , mp.sakuhin_tag_code12
+                    , mp.sakuhin_tag_code13
+                    , mp.sakuhin_tag_code14
+                    , mp.sakuhin_tag_code15
+                    , mp.sakuhin_tag_code16
+                    , mp.sakuhin_tag_code17
+                    , mp.sakuhin_tag_code18
+                    , mp.sakuhin_tag_code19
+                    , mp.sakuhin_tag_code20
+                    , anime.tag_map_code 
+                from
+                    m_anime_title anime 
+                    left join m_sakuhin_tag_map mp 
+                        on mp.tag_map_code = anime.tag_map_code 
+                        and anime.invalid_flg = false 
+                where
+                    anime_title_name LIKE %(title_name)s
+            """
+        return self.selectWithParam(sql, param)
+    def insert(self,param):
+        sql =   """
+                insert 
+                    into m_sakuhin_tag_map( 
+                        tag_map_code
+                        ,title_category_code
+                        , core_code1
+                        , core_code2
+                        , sakuhin_tag_code1
+                        , sakuhin_tag_code2
+                        , sakuhin_tag_code3
+                        , sakuhin_tag_code4
+                        , sakuhin_tag_code5
+                        , sakuhin_tag_code6
+                        , sakuhin_tag_code7
+                        , sakuhin_tag_code8
+                        , sakuhin_tag_code9
+                        , sakuhin_tag_code10
+                        , sakuhin_tag_code11
+                        , sakuhin_tag_code12
+                        , sakuhin_tag_code13
+                        , sakuhin_tag_code14
+                        , sakuhin_tag_code15
+                        , sakuhin_tag_code16
+                        , sakuhin_tag_code17
+                        , sakuhin_tag_code18
+                        , sakuhin_tag_code19
+                        , sakuhin_tag_code20
+                        , invalid_flg
+                        , create_user
+                        , create_time
+                        , update_user
+                        , update_time
+                    ) 
+                    values 
+                    (
+                        %(tag_map_code)s
+                        ,%(title_category_code)s
+                        , %(core_code1)s
+                        , %(core_code2)s
+                        , %(tag_1)s
+                        , %(tag_2)s
+                        , %(tag_3)s
+                        , %(tag_4)s
+                        , %(tag_5)s
+                        , %(tag_6)s
+                        , %(tag_7)s
+                        , %(tag_8)s
+                        , %(tag_9)s
+                        , %(tag_10)s
+                        , %(tag_11)s
+                        , %(tag_12)s
+                        , %(tag_13)s
+                        , %(tag_14)s
+                        , %(tag_15)s
+                        , %(tag_16)s
+                        , %(tag_17)s
+                        , %(tag_18)s
+                        , %(tag_19)s
+                        , %(tag_20)s
+                        , false
+                        , %(full_name)s
+                        , now()
+                        , %(full_name)s
+                        , now()
+                    )
+
+            
+                """
+        return self.updateWithParam(sql,param)
+
+    def update(self,param):
+        sql =   """
+            update m_sakuhin_tag_map
+                set
+                title_category_code = %(title_category_code)s,
+                core_code1 = %(core_code1)s,
+                core_code2 = %(core_code2)s,
+                sakuhin_tag_code1 = %(tag_1)s,
+                sakuhin_tag_code2 = %(tag_2)s,
+                sakuhin_tag_code3 = %(tag_3)s,
+                sakuhin_tag_code4 = %(tag_4)s,
+                sakuhin_tag_code5 = %(tag_5)s,
+                sakuhin_tag_code6 = %(tag_6)s,
+                sakuhin_tag_code7 = %(tag_7)s,
+                sakuhin_tag_code8 = %(tag_8)s,
+                sakuhin_tag_code9 = %(tag_9)s,
+                sakuhin_tag_code10 = %(tag_10)s,
+                sakuhin_tag_code11 = %(tag_11)s,
+                sakuhin_tag_code12 = %(tag_12)s,
+                sakuhin_tag_code13 = %(tag_13)s,
+                sakuhin_tag_code14 = %(tag_14)s,
+                sakuhin_tag_code15 = %(tag_15)s,
+                sakuhin_tag_code16 = %(tag_16)s,
+                sakuhin_tag_code17 = %(tag_17)s,
+                sakuhin_tag_code18 = %(tag_18)s,
+                sakuhin_tag_code19 = %(tag_19)s,
+                sakuhin_tag_code20 = %(tag_20)s,
+                update_user = %(full_name)s,
+                update_time = now()
+            where 
+                tag_map_code = %(tag_map_code)s
+                """
+        return self.updateWithParam(sql,param)
+
+    def selectMaxTagMapCode(self):
+        sql =   """
+                SELECT coalesce(max(tag_map_code),'00000000')
+                FROM m_sakuhin_tag_map
+                """
+
+        return self.select(sql)[0][0]
+
+    def updateTagCodeByTitleCode(self,param_list):
+        tb_name = "01"
+        if param_list['title_category_code'] == '01':
+            tb_name = 'm_manga_title'
+        elif param_list['title_category_code'] == '02':
+            tb_name = 'm_novel_title'
+        else:
+            tb_name = 'm_anime_title'
+
+        param = {
+            'title_code': param_list['title_code'],
+            'tap_map_code': param_list['tag_map_code'],
+            'full_name':param_list['full_name']
+        }
+
+        sql =   """
+                update {}
+                set
+                    tag_map_code = %(tap_map_code)s
+                    ,update_user = %(full_name)s
+                    ,update_time = now()
+                where
+                    manga_title_code = %(title_code)s
+
+                """
+        sql = sql.format(tb_name)
+        return self.updateWithParam(sql,param)
---
+++ code/service_21011_sakuhin_tag_bind.py
@@ -0,0 +1,124 @@
+from admin_app.service.service_main import ServiceMain
+from admin_app.service.service_list import ServiceList
+
+from admin_app.dao.dao_m_core import DaoMCore
+from admin_app.dao.dao_m_sakuhin_tag import DaoMSakuhinTag
+from admin_app.dao.dao_m_sakuhin_tag_category import DaoMSakuhinTagCategory
+
+from admin_app.dao.dao_m_manga_title import DaoMMangaTitle
+from admin_app.dao.dao_m_novel_title import DaoMNovelTitle
+from admin_app.dao.dao_m_anime_title import DaoMAnimeTitle
+from admin_app.dao.dao_m_app_title import DaoMAppTitle
+from admin_app.dao.dao_m_game_title import DaoMGameTitle
+from admin_app.dao.dao_m_sakuhin_tag_bind import DaoMTagBind
+from admin_app.dao.dao_t_update_history import DaoTUpdateHistory
+
+from admin_app.dto.dto_10_sakuhin_tag.dto_21011_sakuhin_tag_bind import DtoSakuhinTagBind
+
+class SakuhinTagBindService(ServiceList):
+
+    # コアマスタテーブル用DAO
+    dao_m_core = DaoMCore()
+    # 作品タグマスタテーブル用DAO
+    dao_m_sakuhin_tag = DaoMSakuhinTag()
+    # 作品タグカテゴリマスタテーブル用DAO
+    dao_m_sakuhin_tag_category = DaoMSakuhinTagCategory()
+    dao_m_sakuhin_tag_bind = DaoMTagBind()
+    dao_t_update_history = DaoTUpdateHistory()
+
+    '''初期描画'''
+    def initialize(self, table_name):
+
+        # 更新履歴
+        notice_table = self.getnoticetable(table_name)
+        dto_core = self.mapping(DtoSakuhinTagBind.CoreAll, self.dao_m_core.selectAll())
+        dto_sakuhin_tag_category = self.mapping(DtoSakuhinTagBind.SakuhinTagCategoryAll, self.dao_m_sakuhin_tag_category.selectSakuhinTagCategoryAll())
+        # 領域用DTOを画面DTOに詰める
+        dto_sakuhin_tag_bind= DtoSakuhinTagBind(notice_table, dto_core, dto_sakuhin_tag_category)
+        return self.unpack(dto_sakuhin_tag_bind)
+
+    def getTitleByName(self, title_name, category_list):
+        '''
+        @param  title_name 検索タイトル
+                category_list 検索カテゴリ（マンガ、小説、アニメ）
+        '''
+        param = {'title_name' : '%' + title_name + '%'}
+
+        manga_list = []
+        novel_list = []
+        anime_list = []
+        
+        for category in category_list:
+            if category == 'manga':
+                # マンガタイトルデータ取得
+                manga_list = self.dao_m_sakuhin_tag_bind.selectMangaTagByTitleName(param)
+            if category == 'novel':
+                # 小説タイトルデータ取得
+                novel_list = self.dao_m_sakuhin_tag_bind.selectNovelTagByTitleName(param)
+            if category == 'anime':
+                # アニメタイトルデータ取得
+                anime_list = self.dao_m_sakuhin_tag_bind.selectAnimeTagByTitleName(param)
+
+        # タイトル一覧生成
+        title_list = tuple(manga_list) + tuple(novel_list) + tuple(anime_list)
+        dto_title_list = self.mapping(DtoSakuhinTagBind.TitleSearchModal, title_list)
+        return self.unpack(dto_title_list)
+    def getTagNameByCode(self,tag_code_list):
+        
+        tag_code_list = tuple(tag_code_list.values())
+        param = {
+            'tag_code_list':tag_code_list
+        }
+        dto_tag_name_list = self.dao_m_sakuhin_tag_bind.getTagNameByCode(param)
+        
+        return self.unpack(dto_tag_name_list)
+    
+    def createInsertOrUpdateData(self,param,full_name):
+        tag_map_code = param['tag_map_code']
+        param_list ={
+            'tag_map_code' :param['tag_map_code'],          
+            'title_category_code':param['title_category_code'],
+            'title_code':param['title_code'],
+            'core_code1':param['core_code1'],
+            'core_code2':param['core_code2'],
+            'tag_1':param['sakuhin_tagArea1']['tag-1'],
+            'tag_2':param['sakuhin_tagArea1']['tag-2'],
+            'tag_3':param['sakuhin_tagArea1']['tag-3'],
+            'tag_4':param['sakuhin_tagArea1']['tag-4'],
+            'tag_5':param['sakuhin_tagArea1']['tag-5'],
+            'tag_6':param['sakuhin_tagArea2']['tag-6'],
+            'tag_7':param['sakuhin_tagArea2']['tag-7'],
+            'tag_8':param['sakuhin_tagArea2']['tag-8'],
+            'tag_9':param['sakuhin_tagArea2']['tag-9'],
+            'tag_10':param['sakuhin_tagArea2']['tag-10'],
+            'tag_11':param['sakuhin_tagArea3']['tag-11'],
+            'tag_12':param['sakuhin_tagArea3']['tag-12'],
+            'tag_13':param['sakuhin_tagArea3']['tag-13'],
+            'tag_14':param['sakuhin_tagArea3']['tag-14'],
+            'tag_15':param['sakuhin_tagArea3']['tag-15'],
+            'tag_16':param['sakuhin_tagArea3']['tag-16'],
+            'tag_17':param['sakuhin_tagArea3']['tag-17'],
+            'tag_18':param['sakuhin_tagArea3']['tag-18'],
+            'tag_19':param['sakuhin_tagArea3']['tag-19'],
+            'tag_20':param['sakuhin_tagArea3']['tag-20'],
+            'full_name' : full_name
+        }
+
+        if tag_map_code == '':
+            # m_sakuhin_tap_mapに存在するtag_map_codeの最大値を取得
+            new_code = self.dao_m_sakuhin_tag_bind.selectMaxTagMapCode()
+            # 最大値+1を0埋めする（新規tag_map_code）
+            max_code = str(int(new_code[2:])+1).zfill(10)
+            param_list['tag_map_code'] = max_code
+            self.dao_m_sakuhin_tag_bind.insert(param_list)
+
+            # タイトルマスタのタグマップコードを更新
+            self.dao_m_sakuhin_tag_bind.updateTagCodeByTitleCode(param_list)
+            # (変更テーブル名,操作内容,変更対象,備考,更新者)を設定
+            entity = ('m_sakuhin_tag_map','追加',param['title_name'],'',full_name)
+            self.dao_t_update_history.insert(entity)
+
+        else:
+            entity = ('m_sakuhin_tag_map','編集',param['title_name'],'',full_name)
+            self.dao_t_update_history.insert(entity)
+            self.dao_m_sakuhin_tag_bind.update(param_list)
